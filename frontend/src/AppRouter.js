5a0617ecdcacc65dcee4f4d5894b6374ef35bef0
ferasaljoudi <feras.aljoudi@gmail.com>
2024-05-19 14:19:37 -0600

    Adding frontend

---
+++ frontend/src/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Welcome from './components/Welcome';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;

76395390d48a60bdac1908c7ebd556c6ac749614
Feras Aljoudi <feras.aljoudi@gmail.com>
2024-06-23 12:39:38 -0600

    update

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import SignedInHeader from './SignedInHeader';
+import { useNavigate, useLocation } from 'react-router-dom';
+import './Profile.css';
 
 function Profile() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { email } = location.state || {};
+
+    const handleLogout = () => {
+        navigate('/');
+    };
+    
     return (
         <div>
         <SignedInHeader />
         <div className="content">
-            <h1>Profile</h1>
-            {/* Profile content */}
+            <h1>{email} Profile</h1>
+            <button onClick={handleLogout} className="logout-button">Logout</button>
         </div>
         </div>
     );

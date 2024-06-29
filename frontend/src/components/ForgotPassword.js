63a8c027d7b3d4522b345f350d23a9d3406efdf6
Feras Aljoudi <feras.aljoudi@gmail.com>
2024-06-29 12:11:34 -0600

    update forgotPasswordPage

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -5,18 +5,21 @@ import Header from './Header';
 
 function ForgotPassword() {
   const [email, setEmail] = useState('');
-  const [code, setCode] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
+  const [resetMessage, setResetMessage] = useState('');
   const [step, setStep] = useState(1);
   const navigate = useNavigate();
 
   const handleEmailSubmit = async (e) => {
     e.preventDefault();
 
+    if (!email) {
+      setError('Email is required!');
+      return;
+    }
+
     try {
-      const response = await fetch('http://localhost:8080/api/users/forgot-password', {
+      const response = await fetch('http://localhost:8080/api/users/resetPassword', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -25,6 +28,8 @@ function ForgotPassword() {
       });
 
       if (response.ok) {
+        const result = await response.text();
+        setResetMessage(result);
         setStep(2);
         setError('');
       } else {
@@ -32,59 +37,7 @@ function ForgotPassword() {
         setError(result);
       }
     } catch (error) {
-      setError('An error occurred while sending the reset code');
-    }
-  };
-
-  const handleCodeSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch('http://localhost:8080/api/users/verify-reset-code', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, resetCode: code }),
-      });
-
-      if (response.ok) {
-        setStep(3);
-        setError('');
-      } else {
-        const result = await response.text();
-        setError(result);
-      }
-    } catch (error) {
-      setError('An error occurred while verifying the reset code');
-    }
-  };
-
-  const handlePasswordSubmit = async (e) => {
-    e.preventDefault();
-
-    if (newPassword !== confirmPassword) {
-      setError('Passwords do not match');
-      return;
-    }
-
-    try {
-      const response = await fetch('http://localhost:8080/api/users/reset-password', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, newPassword, confirmPassword }),
-      });
-
-      if (response.ok) {
-        navigate('/');
-      } else {
-        const result = await response.text();
-        setError(result);
-      }
-    } catch (error) {
-      setError('An error occurred while resetting the password');
+      setError('An error occurred while sending the reset link');
     }
   };
 
@@ -102,39 +55,20 @@ function ForgotPassword() {
               placeholder="Email Address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="forgot-password-input"
             />
-            <button type="submit">Send Code</button>
+            <div className="forgot-password-button-container">
+              <button type="button" onClick={() => navigate('/')} className="forgot-password-back-button">BACK</button>
+              <button type="submit" className="forgot-password-button">Send Link</button>
+            </div>
           </form>
         )}
 
         {step === 2 && (
-          <form onSubmit={handleCodeSubmit}>
-            <input
-              type="text"
-              placeholder="Enter Code"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-            />
-            <button type="submit">Verify Code</button>
-          </form>
-        )}
-
-        {step === 3 && (
-          <form onSubmit={handlePasswordSubmit}>
-            <input
-              type="password"
-              placeholder="New Password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button type="submit">Reset Password</button>
-          </form>
+          <div>
+            <p>{resetMessage}</p>
+            <button onClick={() => navigate('/')} className="forgot-password-button2">Go to Home</button>
+          </div>
         )}
       </div>
     </div>
